30131faab74bb1bc8deb44e279f89dadc02a74f9
Gabrielli <gabrielliborba42@gmail.com>
2026-03-20 07:37:18 -0300

    feat: initial criação do projeto

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+export function App() {
+  const [list, setList] = useState([
+    {id: '1', label:"Fazer café"},
+    {id: '2', label:"Fazer café"},
+    {id: '3', label:"Fazer almoço "},
+    {id: '4', label:"Fazer janta"},
+
+  ]);
+
+  return (
+    <div>
+      <input/>
+      <button>
+        Adicionar
+      </button>
+      
+    <ol>
+      {list.map(listItem => (
+        <li key={listItem.id}>
+          {listItem.label}
+        </li>
+      ))}
+    </ol>
+
+    </div>
+  )
+}
+
