d7c71e007f91e34310c373337fd9f40603155c64
Gabrielli <gabrielliborba42@gmail.com>
2026-04-08 07:46:46 -0300

    Organiza estrutura do projeto Task Flow separando frontend e backend e adiciona estrutura inicial do Spring Boot

---
+++ taskflow_backend/src/test/java/com/gabrielli/taskflow_backend/TaskflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielli.taskflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
