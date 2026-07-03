1b51cf9bd28412fc5b2b0db5b8f03310d987e798
Gabrielli <gabrielliborba42@gmail.com>
2026-07-03 08:16:06 -0300

    feat: Adiciona componente Layout.tsx e Layout.css para aplicar tamanho igual de largura para todas as páginas do sistema. Inseri no AppRoutes o Componente de Layout para que fosse herdado a todas as paginas da rota

--- taskflow_frontend/src/routes/AppRoutes.tsx
+++ taskflow_frontend/src/routes/AppRoutes.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home  from "../pages/Home";
 import Pendentes from "../pages/Pendentes";
 import { Concluidas } from "../pages/Concluidas";
+import Layout from "../components/Layout";
 
 export function AppRoutes() {
   return (
 
     <BrowserRouter>
+     <Layout>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Pendentes" element={<Pendentes />} />
             <Route path="/Concluidas" element={<Concluidas />} />
         </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
